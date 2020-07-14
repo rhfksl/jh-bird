8b5f1b1ba0b5747623acea3f224ba80d09d15851
rhfksl <wkaqkdrn@gmail.com>
2020-07-14 18:51:48 +0900

    ⚡️reducer 구조 변경 및 채팅방 실시간 업데이트
     - 실시간으로 채팅이 왔을 시 채팅방 새로 생성
     - 최근의 채팅방이 가장 상단으로 오도록 설정
     - 리듀서 구조 변경
     - 새로 생긴 채팅방이 store에 존재하지 않을 시 새로 생성

--- src/screens/ChatRoomListScreen.js
+++ src/screens/ChatRoomListScreen.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Image } from 'react-native';
 import { connect } from 'react-redux';
 import shortid from 'shortid';
 
-function ChatRoomListScreen({ navigation, allMessages, currentChatRooms, user, friendLists }) {
+function ChatRoomListScreen({ navigation, allMessages, currentChatRoomlists, user, friendLists }) {
   const [chatRoomLists, setChatRoomLists] = useState([]);
 
-  const getFriendNickname = (roomInfo) => {
+  const getRoomName = (roomInfo) => {
     if (roomInfo.userId === user.id) {
       return { roomname: roomInfo.roomname2, friendId: roomInfo.userId2 };
     } else {
@@ -14,12 +14,33 @@ function ChatRoomListScreen({ navigation, allMessages, currentChatRooms, user, f
     }
   };
 
+  const getFriendImage = (friendId) => {
+    let lists = Object.values(friendLists);
+    let leng = lists.length;
+    for (let i = 0; i < leng; i++) {
+      if (lists[i].id === friendId) {
+        return lists[i].img;
+      }
+    }
+  };
+
+  const formatTimestamp = (time) => {
+    time = time.split('T');
+    let date = time[0];
+    let hour = time[1].substr(0, 8);
+    return { date, hour };
+  };
+
   const makeChatRoomComponent = (roomId, roomInfo) => {
-    const { friendId, roomname } = getFriendNickname(roomInfo);
+    const { friendId, roomname } = getRoomName(roomInfo);
+
+    const image = getFriendImage(friendId);
+    let { date, hour } = formatTimestamp(roomInfo.messages[0].createdAt);
+
     return (
       <TouchableOpacity
         key={shortid.generate()}
-        style={{ backgroundColor: 'yellow', height: 70, marginBottom: 10 }}
+        style={styles.chattingRoomList}
         onPress={() => {
           navigation.navigate('chatting', {
             chattingRoomId: roomId,
@@ -27,32 +48,61 @@ function ChatRoomListScreen({ navigation, allMessages, currentChatRooms, user, f
           });
         }}
       >
-        <Text>{roomname}</Text>
+        <Image style={styles.img} source={{ url: image }} />
+        <View style={styles.room}>
+          <Text>{roomname}</Text>
+          <View style={{ alignItems: 'flex-end', paddingRight: 10 }}>
+            <Text style={{ color: 'gray' }}>{date}</Text>
+            <Text style={{ color: 'gray' }}>{hour}</Text>
+          </View>
+        </View>
       </TouchableOpacity>
     );
   };
 
   useEffect(() => {
-    if (currentChatRooms.length) {
+    if (currentChatRoomlists.length) {
       const changeRoomOrder = [];
-      for (let room of currentChatRooms) {
+      for (let room of currentChatRoomlists) {
         changeRoomOrder.push(makeChatRoomComponent(room, allMessages[room]));
       }
       setChatRoomLists(changeRoomOrder);
     }
-  }, [currentChatRooms]);
+  }, [currentChatRoomlists]);
 
   return (
-    <View style={{ flex: 1, backgroundColor: 'green' }}>
+    <View style={{ flex: 1, backgroundColor: 'transparent' }}>
       <ScrollView>{chatRoomLists}</ScrollView>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  chattingRoomList: {
+    paddingLeft: 10,
+    height: 70,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  img: {
+    width: 50,
+    height: 50,
+    borderRadius: 55,
+  },
+  room: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flex: 1,
+    paddingLeft: 10,
+  },
+});
+
 function mapReduxStateToReactProps(state) {
   return {
     allMessages: state.allMessages,
-    currentChatRooms: state.currentChatRooms,
+    currentChatRoomlists: state.currentChatRoomlists,
     user: state.user,
     friendLists: state.friendLists,
   };
